fbe619f625d7ed7f8fe3bf4172555aa2998478d4
Andy Yoon <andy@infograb.net>
2025-02-24 14:20:13 +0900

    Remove unused writeFile import in PandocMdTo node

--- nodes/PandocMdTo/PandocMdTo.node.ts
+++ nodes/PandocMdTo/PandocMdTo.node.ts
@@ -6,7 +6,7 @@ import {
 	IBinaryKeyData,
 	NodeOperationError,
 } from 'n8n-workflow';
-import { writeFile, readFile, unlink, rmdir } from 'fs/promises';
+import { readFile, unlink, rmdir } from 'fs/promises';
 import { join } from 'path';
 import { v5 as uuidv5 } from 'uuid';
 import { exec as execCallback } from 'child_process';
